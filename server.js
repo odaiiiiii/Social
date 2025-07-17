8f7a30b0279505371acc38a0f9d176fef7b70539
ODAI10 <odaiadawi12@gmail.com>
2025-07-17 16:11:12 +0300

    test

--- server.js
+++ server.js
@@ -16,7 +16,8 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    // origin: "http://localhost:5173",
+     origin: "*",
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     credentials: true,
   })
