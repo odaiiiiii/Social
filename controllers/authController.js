291aad08bcd9e30bde08721b9588be14d50c7925
Odai <odaiabd48@gmail.com>
2025-07-20 22:58:52 +0300

    add

--- controllers/authController.js
+++ controllers/authController.js
@@ -81,8 +81,8 @@ const logout = async (req, res) => {
   try {
     res.clearCookie("token", {
       httpOnly: true,
-      sameSite: "Lax",
-      secure: process.env.NODE_ENV === "production",
+      sameSite: "None",
+      secure: true,
     });
 
     res.json({ message: "Logged out successfully" });
